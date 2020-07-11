a5d00e756d777857b9d51c8e4e0489c4e413fe25
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-11 21:23:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,8 +7,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.each do |word|
-      word.reverse
-      new_array.push(word)
+      new_array.push(word.reverse)
   end
   new_array
 end
